0a3a97e4c0c5dd2c6081f140825d80d7559988ff
NicholasNPham <phamn1000@gmail.com>
2025-11-03 21:57:05 -0500

    skeleton daemon v2

--- autopop.py
+++ autopop.py
@@ -1,18 +1,27 @@
 import time
 from datetime import datetime
 
+last_run_day = None
+
 while True:
     try:
         today = datetime.today().weekday()
+        current_day = today
         print(today)
-        if today == 5: # 5 Meaning Saturday
-            pass
-            # This is where the Notion API Logic Goes
 
-        time.sleep(43200)
-    except KeyboardInterrupt:
+        if today == 5: # 5 Meaning Saturday
+            if last_run_day != today:
+                pass
+
+                # API NOTION CALL
+
+                last_run_day = today # if it's the same day in 12 hours do not run
+                print("AUTO POPULATED NOTION SCHEDULE")
+
+        time.sleep(43200) # Check after 12 Hours
+    except KeyboardInterrupt: # Keyboard Interruption
         print("Stopped")
-        break
+        break # User Broke the Script
     except Exception as error:
         print(error)
-        time.sleep(3600)
+        time.sleep(3600) # If Error Check Every 1 Hour
