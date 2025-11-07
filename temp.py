461fd6534b7bb0b89826910d0919e4d61b7f0c35
NicholasNPham <phamn1000@gmail.com>
2025-11-06 21:25:35 -0500

    date work

--- temp.py
+++ temp.py
@@ -1,2 +1,23 @@
 # Working on connection to Notion API need to download notion api client
 
+from datetime import timedelta, datetime, timezone
+
+
+
+today = datetime.today() # YYYY-MM-DD HH:MM:SS.MMMMMM
+num_of_today = today.weekday() # var set a num of day: e.g. monday = 0, tuesday = 1 ...
+
+shift_dict = {}
+
+for i in range(2, 7):
+    today += timedelta(days=1)
+    shift_dict["SAO10 SHIFT " + str(i - 1)] = [today.strftime("%Y-%m-%d") + "T08:00:00" + today.strftime("%z"), today.strftime("%Y-%m-%d") + "T17:00:00" + today.strftime("%z")]
+
+for key, value in shift_dict.items():
+    print(f"{key} : {value}")
+
+# Need to fix for timezone we have daylight savings in florida. EST
+
+# print(f"'{today}' variable type: {type(today)}")
+# print(f"'{num_of_today}' variable type: {type(num_of_today)}")
+
