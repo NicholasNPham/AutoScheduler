9dd09eff965051210e278b50c54c3306aa647013
Nicholas Pham <npham@sao10.com>
2025-11-07 10:59:03 -0500

    fixed issue with timezone and date

--- temp.py
+++ temp.py
@@ -1,22 +1,24 @@
 # Working on connection to Notion API need to download notion api client
 
-from datetime import timedelta, datetime, timezone
+from datetime import timedelta, datetime
+import zoneinfo
 
-
-
-today = datetime.today() # YYYY-MM-DD HH:MM:SS.MMMMMM
+timezone = zoneinfo.ZoneInfo('America/New_York')
+today = datetime.now(timezone) # YYYY-MM-DD HH:MM:SS.MMMMMM
 num_of_today = today.weekday() # var set a num of day: e.g. monday = 0, tuesday = 1 ...
 
 shift_dict = {}
 
+today_ahead = today + timedelta(days=1)
+
 for i in range(2, 7):
-    today += timedelta(days=1)
-    shift_dict["SAO10 SHIFT " + str(i - 1)] = [today.strftime("%Y-%m-%d") + "T08:00:00" + today.strftime("%z"), today.strftime("%Y-%m-%d") + "T17:00:00" + today.strftime("%z")]
+    today_ahead += timedelta(days=1)
+    start = today_ahead.strftime("%Y-%m-%d") + "T08:00:00" + today_ahead.strftime("%z")
+    end = today_ahead.strftime("%Y-%m-%d") + "T17:00:00" + today_ahead.strftime("%z")
+    shift_dict["SAO10 SHIFT " + str(i - 1)] = [start[1:22] + ":" + start[22:], end[1:22] + ":" + end[22:]]
 
 for key, value in shift_dict.items():
     print(f"{key} : {value}")
-
-# Need to fix for timezone we have daylight savings in florida. EST
 
 # print(f"'{today}' variable type: {type(today)}")
 # print(f"'{num_of_today}' variable type: {type(num_of_today)}")
