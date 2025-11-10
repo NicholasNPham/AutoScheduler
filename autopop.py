b65a2884d9e0005164c469770c3ec8ed9be10484
NicholasNPham <npham@sao10.com>
2025-11-10 11:27:21 -0500

    date checker is functional, will need to run for a week to test

--- autopop.py
+++ autopop.py
@@ -1,5 +1,6 @@
 import time
-from datetime import datetime
+from datetime import timedelta, datetime
+import zoneinfo
 
 last_run_day = None
 
@@ -11,9 +12,23 @@ while True:
 
         if today == 5: # 5 Meaning Saturday
             if last_run_day != today:
-                pass
 
-                # API NOTION CALL
+                timezone = zoneinfo.ZoneInfo('America/New_York')
+                today = datetime.now(timezone)  # YYYY-MM-DD HH:MM:SS.MMMMMM
+                num_of_today = today.weekday()  # var set a num of day: e.g. monday = 0, tuesday = 1 ...
+
+                shift_dict = {}
+
+                today_ahead = today + timedelta(days=1)
+
+                for i in range(2, 7):
+                    today_ahead += timedelta(days=1)
+                    start = today_ahead.strftime("%Y-%m-%d") + "T08:00:00" + today_ahead.strftime("%z")
+                    end = today_ahead.strftime("%Y-%m-%d") + "T17:00:00" + today_ahead.strftime("%z")
+                    shift_dict["SAO10 SHIFT " + str(i - 1)] = [start[1:22] + ":" + start[22:], end[1:22] + ":" + end[22:]]
+
+                for key, value in shift_dict.items():
+                    print(f"{key} : {value}")
 
                 last_run_day = today # if it's the same day in 12 hours do not run
                 print("AUTO POPULATED NOTION SCHEDULE")
