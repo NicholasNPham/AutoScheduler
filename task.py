9550b84981cbed886811ae171af8537f81f980ac
NicholasNPham <phamn1000@gmail.com>
2026-01-12 20:52:18 -0500

    started new file with notes

---
+++ task.py
@@ -0,0 +1,21 @@
+"""
+This is going to be the start of a new database scheduler.
+- This file is going to create tasks that are mandatory on weekdays.
+- This will include classes and human-capital tasks that are necessary
+- The Classes:
+    - Either can have two or one classes depended on the term (Spring, Fall, Summer)
+    - They usually fall after work and after break
+    - They are usually 1 hour time blocks.
+- The Human-Capital Classes:
+    - Can have multiple but lets have two or one. (falling on a weekday specifically)
+    - They also fall after work and after break
+    - They are usually 1 hour time blocks.
+
+Notes, Classes are priority 1 meaning that they are the earliest part of the day
+Human-Capital Tasks are priority two meaning that they are the second earliest part of the day.
+
+This will be coded within NASA Coding Requirements
+Reasons:
+- Looks good and I want to.
+- To have fun and just to code.
+"""
